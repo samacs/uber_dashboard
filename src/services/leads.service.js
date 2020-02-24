59e2fe99d8655f9de891ba626b8e37ecb6455c6f
Saul Martartínez <saul.martinez05@gmail.com>
2020-02-24 11:28:17 -0600

    Remove `console.log`.

--- src/services/leads.service.js
+++ src/services/leads.service.js
@@ -2,7 +2,6 @@ import Api from './api'
 
 export default class LeadsService extends Api {
   submitLead = (locationUrn, leadForm, lead) => {
-    console.log(lead)
     return this.post(
       `/locations/${locationUrn}/html_forms/${leadForm}/submissions`,
       lead,
